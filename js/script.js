57aab2f56007c8011878314363344845c1166c86
Desmond Chin <chin.sy.des@gmail.com>
2018-09-06 10:59:44 +0800

    put stop on detect back

--- js/script.js
+++ js/script.js
@@ -24,4 +24,5 @@ Quagga.onDetected(function (data) {
     console.log('on detected');
     document.getElementById('result').innerHTML = data.codeResult.code;
     console.log(data.codeResult.code);
+    Quagga.stop();
 });
